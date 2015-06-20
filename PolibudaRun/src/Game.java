23b3218b5161d7cc7a529eeb7b85c1fde5dcb18f
MatuszUss <mateuszuss1994@wp.pl>
2015-06-20 14:08:43 +0100

    next world

--- PolibudaRun/src/Game.java
+++ PolibudaRun/src/Game.java
@@ -1,18 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
 public class Game {
+	List<int[][]> worlds = new ArrayList<int[][]>();
 
-	public Game(JFrame frame, Menu menu){
-		
+	// potem do wyrzucenia ta lista
+
+	public Game(JFrame frame, Menu menu) {
+
 	}
-	
-	public void start(){
-		
+
+	public void start() {
+
 	}
-	
-	public void fillBoard(JLabel[][] board, int[][] world, Character character){
-		
+
+	public void fillBoard(JLabel[][] board, int[][] world, Character character) {
+
+	}
+
+	// czy pole aktualny swiat czy jest ostatnie to error koniec swiata
+	// jesli nie to wczytaj ten swiat metodá fillworld throw exception opis 1
+	// zdanie
+
+	public void CzyJestNastepnySwiat(JLabel[][] etykieta, int[][] world,
+			Character character) {
+		if (this.worlds.size() == worldId) {
+			throw new Exception("brak swiata"); // brak kolejnego swiata
+			return;
+		}
+		fillBoard(etykieta, world, character);
+		worldId++;
 	}
 }
